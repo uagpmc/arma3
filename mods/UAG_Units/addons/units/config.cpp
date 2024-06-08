87e2729dc064700b2cce3a102df441e6d3006695
zuedev <zuedev@gmail.com>
2024-06-08 19:06:46 +0100

    add more gear to the UAG units

--- mods/UAG_Units/addons/units/config.cpp
+++ mods/UAG_Units/addons/units/config.cpp
@@ -12,10 +12,87 @@ class CfgPatches {
     };
 };
 
+class CfgVehicles {
+    class B_CTRG_Soldier_urb_1_F;
+
+    class UAG_Combat_Fatigues_Soldier: B_CTRG_Soldier_urb_1_F {
+        hiddenSelectionsTextures[] = {"z\UAG_Units\addons\units\data\clothing1_mtp_urban_co_uag.paa"};
+        picture = "z\UAG_Units\addons\units\data\icon_U_B_CTRG_Soldier_urb_1_F_ca_uag.paa";
+    };
+
+    class Supply0;
+    
+    class Supply280: Supply0 {
+        maximumLoad = 280;
+    };
+};
+
 class CfgWeapons {
     class HitpointsProtectionInfo;
     class Head;
     class Face;
+
+    // UAG Combat Fatigues
+    // Based on: U_B_CTRG_Soldier_urb_1_F
+    // Changes:
+    // - Double storage
+    // - Weightless
+    class U_B_CTRG_Soldier_urb_1_F;
+
+    class U_B_CTRG_Soldier_urb_1_F_PROXY: U_B_CTRG_Soldier_urb_1_F {
+        scope = 0;
+        class ItemInfo;
+    };
+
+    class UAG_Combat_Fatigues: U_B_CTRG_Soldier_urb_1_F_PROXY {
+        scope = 2;
+        displayName = "UAG Combat Fatigues";
+        hiddenSelectionsTextures[] = {"z\UAG_Units\addons\units\data\clothing1_mtp_urban_co_uag.paa"};
+        picture = "z\UAG_Units\addons\units\data\icon_U_B_CTRG_Soldier_urb_1_F_ca_uag.paa";
+
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply80";
+            mass = 0;
+            uniformClass = "UAG_Combat_Fatigues_Soldier";
+        };
+    };
+
+    // UAG Plate Carrier - Light
+    // Based on: V_PlateCarrier1_blk
+    // Changes:
+    // - Double storage
+    // - Quarter weight
+    // - Double armour hitpoints
+    class V_PlateCarrier1_blk;
+
+    class V_PlateCarrier1_blk_PROXY: V_PlateCarrier1_blk {
+        scope = 0;
+        class ItemInfo;
+    };
+
+    class UAG_Plate_Carrier_Light: V_PlateCarrier1_blk_PROXY {
+        scope = 2;
+        displayName = "UAG Plate Carrier - Light";
+        hiddenSelectionsTextures[] = {"z\UAG_Units\addons\units\data\vests_blk_co_uag.paa"};
+        picture = "z\UAG_Units\addons\units\data\icon_v_platecarrier1_blk_ca_uag.paa";
+
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply280";
+            mass = 20;
+
+            class HitpointsProtectionInfo: HitpointsProtectionInfo {
+                class Abdomen: Abdomen {
+                    armor = 32;
+                };
+                class Chest: Chest {
+                    armor = 32;
+                };
+                class Diaphragm: Diaphragm {
+                    armor = 32;
+                };
+            };
+        };
+    };
 
     // UAG Combat Helmet - Heavy
     // Based on: H_HelmetB_TI_tna_F
