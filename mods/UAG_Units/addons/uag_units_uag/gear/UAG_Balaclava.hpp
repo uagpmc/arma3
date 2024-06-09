f909574232587adf3569f6ef1e7b2e40d5e98621
zuedev <zuedev@gmail.com>
2024-06-09 13:53:19 +0100

    add uag goggles + balaclava combos

---
+++ mods/UAG_Units/addons/uag_units_uag/gear/UAG_Balaclava.hpp
@@ -0,0 +1,17 @@
+/* UAG_Balaclava
+ * Based on: G_Balaclava_TI_tna_F
+ * From: A3_Characters_F_Exp
+ * Changes:
+ * - Weightless
+*/
+
+class CfgGlasses {
+    class G_Balaclava_TI_G_tna_F;
+
+    class UAG_Combat_Goggles: G_Balaclava_TI_G_tna_F {
+        displayName = "UAG Balaclava + Combat Goggles";
+        hiddenSelectionsTextures[] = {"z\UAG_Units\addons\uag_units_uag\data\g_balaclava_ti_tna_f_co_uag.paa"};
+        picture = "z\UAG_Units\addons\uag_units_uag\data\icon_G_Balaclava_TI_tna_F_ca_uag.paa";
+        mass = 0;
+    };
+};
