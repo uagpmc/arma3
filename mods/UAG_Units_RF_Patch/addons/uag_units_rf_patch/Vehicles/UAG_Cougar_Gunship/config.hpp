6e1bec27532a16f48e69e5155517aa45cc65f890
zuedev <zuedev@gmail.com>
2024-06-09 22:20:39 +0100

    fix issues with cougar heli

--- mods/UAG_Units_RF_Patch/addons/uag_units_rf_patch/Vehicles/UAG_Cougar_Gunship/config.hpp
+++ mods/UAG_Units_RF_Patch/addons/uag_units_rf_patch/Vehicles/UAG_Cougar_Gunship/config.hpp
@@ -10,6 +10,7 @@ class CfgVehicles {
 
     class UAG_Cougar_Gunship_PARENTPROXY: I_E_EC_02_RF {
         scope = 0;
+        
         class EventHandlers;
         class Components;
     };
@@ -31,33 +32,41 @@ class CfgVehicles {
 
         class EventHandlers: EventHandlers {
             // this is a hack to get around issues with the vehicle textures not loading properly
-            postinit = "if (local (_this select 0)) then {[(_this select 0), """", [], true] call bis_fnc_initVehicle; (_this select 0) setObjectTextureGlobal [0, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_exterior_03_ldf_co_uag.paa']; (_this select 0) setObjectTextureGlobal [1, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_int_cargo_co_uag.paa']; (_this select 0) setObjectTextureGlobal [3, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_adds_03_ldf_co_uag.paa']; (_this select 0) setObjectTextureGlobal [4, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_exterior_03_ldf_co_uag.paa']; (_this select 0) setObjectTextureGlobal [5, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_int_cargo_co_uag.paa'];};";
+            postinit = "if (local (_this select 0)) then {[(_this select 0), """", [], true] call bis_fnc_initVehicle; (_this select 0) setObjectTextureGlobal [0, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_exterior_03_ldf_co_uag.paa']; (_this select 0) setObjectTextureGlobal [1, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_int_cargo_co_uag.paa']; (_this select 0) setObjectTextureGlobal [3, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_adds_03_ldf_co_uag.paa']; (_this select 0) setObjectTextureGlobal [4, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_exterior_03_ldf_co_uag.paa']; (_this select 0) setObjectTextureGlobal [5, 'z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Cougar_Gunship\as332_int_cargo_co_uag.paa']; (_this select 0) setWaterLeakiness 0;};";
         };
 
         class Components: Components {
-            class TransportPylonsComponent: TransportPylonsComponent {
-                class Presets: Presets {
-                    class Default: Default {
-                        attachment[] = {
-                            "PylonRack_19Rnd_missiles_black_RF",
-                            "PylonRack_19Rnd_PG_missiles_black_RF",
-                            "PylonRack_19Rnd_PG_missiles_black_RF",
-                            "PylonRack_19Rnd_missiles_black_RF"
-                        };
+            class TransportPylonsComponent {
+                uiPicture = "\lxRF\air_rf\heli_medium_ec\data\UI\heli_medium_ec_02_3DEN_CA.paa";
+
+                class Presets {
+                    class HAT {
+                        displayName = "Heavy AT";
+                        attachment[] = {"PylonRack_4Rnd_LG_scalpel","PylonRack_1Rnd_Missile_AGM_01_F","PylonRack_1Rnd_Missile_AGM_01_F","PylonRack_4Rnd_LG_scalpel"};
                     };
                 };
-                class Pylons: Pylons {
-                    class PylonLeft1: PylonLeft1 {
-                        attachment = "PylonRack_19Rnd_missiles_black_RF";
+                class Pylons {
+                    class PylonLeft1 {
+                        attachment = "PylonRack_4Rnd_LG_scalpel";
+                        hardpoints[] = {"O_MISSILE_PYLON","O_BOMB_PYLON_HELI","UNI_SCALPEL","20MM_TWIN_CANNON","WEAPON_PODS_RF"};
+                        priority = 5;
+                        turret[] = {0};
+                        UIposition[] = {0.06,0.4};
                     };
-                    class PylonLeft2: PylonLeft2 {
-                        attachment = "PylonRack_19Rnd_PG_missiles_black_RF";
+                    class PylonLeft2: PylonLeft1 {
+                        attachment = "PylonRack_1Rnd_Missile_AGM_01_F";
+                        priority = 4;
+                        UIposition[] = {0.08,0.35};
                     };
-                    class PylonRight1: PylonRight1 {
-                        attachment = "PylonRack_19Rnd_missiles_black_RF";
+                    class PylonRight1: PylonLeft1 {
+                        attachment = "PylonRack_1Rnd_Missile_AGM_01_F";
+                        mirroredMissilePos = 1;
+                        UIposition[] = {0.59,0.4};
                     };
-                    class PylonRight2: PylonRight2 {
-                        attachment = "PylonRack_19Rnd_PG_missiles_black_RF";
+                    class PylonRight2: PylonLeft2 {
+                        attachment = "PylonRack_4Rnd_LG_scalpel";
+                        mirroredMissilePos = 2;
+                        UIposition[] = {0.57,0.35};
                     };
                 };
             };
