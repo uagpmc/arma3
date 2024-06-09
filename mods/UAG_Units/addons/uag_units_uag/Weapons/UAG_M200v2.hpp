82a2651458f0074b3085178aa825b5c978178c14
zuedev <zuedev@gmail.com>
2024-06-09 16:49:44 +0100

    fix UAG_M200v2_LRPS

--- mods/UAG_Units/addons/uag_units_uag/Weapons/UAG_M200v2.hpp
+++ mods/UAG_Units/addons/uag_units_uag/Weapons/UAG_M200v2.hpp
@@ -1,12 +1,12 @@
 class CfgWeapons {
     class srifle_LRR_F;
 
-    class UAG_M200: srifle_LRR_F {
+    class UAG_M200v2: srifle_LRR_F {
         displayName = "UAG M200v2";
         baseWeapon = "UAG_M200v2";
     };
     
-    class UAG_M200_LRPS: UAG_M200 {
+    class UAG_M200v2_LRPS: UAG_M200 {
         class LinkedItems {
             class LinkedItemsOptic {
                 slot = "CowsSlot";
