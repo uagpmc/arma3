14a36b3c578dedebe151b0d47ac12f7ad587ef03
zuedev <zuedev@gmail.com>
2024-06-09 13:56:14 +0100

    cont. 2

--- mods/UAG_Units/addons/uag_units_uag/gear/UAG_Balaclava.hpp
+++ mods/UAG_Units/addons/uag_units_uag/gear/UAG_Balaclava.hpp
@@ -8,7 +8,7 @@
 class CfgGlasses {
     class G_Balaclava_TI_tna_F;
 
-    class UAG_Combat_Goggles: G_Balaclava_TI_tna_F {
+    class UAG_Balaclava: G_Balaclava_TI_tna_F {
         displayName = "UAG Balaclava";
         hiddenSelectionsTextures[] = {"z\UAG_Units\addons\uag_units_uag\data\g_balaclava_ti_tna_f_co_uag.paa"};
         picture = "z\UAG_Units\addons\uag_units_uag\data\icon_G_Balaclava_TI_tna_F_ca_uag.paa";
