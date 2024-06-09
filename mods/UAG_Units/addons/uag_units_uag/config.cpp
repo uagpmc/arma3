4cb9076c3afbbbb9daa8f04febe37aaa3dbfc24f
zuedev <zuedev@gmail.com>
2024-06-09 13:22:28 +0100

    better uag units structure

---
+++ mods/UAG_Units/addons/uag_units_uag/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches {
+    class uag_units_uag {
+        authors[] = {"zuedev"};
+        authorUrl = "https://uagpmc.com";
+        name = "uag_units_uag";
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "A3_Characters_F_Exp",
+            "A3_Characters_F_Exp_Headgear"
+        };
+        requiredVersion = 2.16;
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+#include "gear/UAG_Combat_Fatigues.hpp"
+#include "gear/UAG_Plate_Carrier_Light.hpp"
+#include "gear/UAG_Combat_Helmet_Heavy.hpp"
---
+++ mods/UAG_Units/addons/uag_units_uag/gear/UAG_Combat_Fatigues.hpp
@@ -0,0 +1,38 @@
+/* UAG Combat Fatigues
+ * Based on: U_B_CTRG_Soldier_urb_1_F
+ * From: A3_Characters_F
+ * Changes:
+ * - Double storage
+ * - Weightless
+ */
+
+class CfgWeapons {
+    class U_B_CTRG_Soldier_urb_1_F;
+
+    class UAG_Combat_Fatigues_PARENTPROXY: U_B_CTRG_Soldier_urb_1_F {
+        scope = 0;
+        class ItemInfo;
+    };
+
+    class UAG_Combat_Fatigues: UAG_Combat_Fatigues_PARENTPROXY {
+        scope = 2;
+        displayName = "UAG Combat Fatigues";
+        hiddenSelectionsTextures[] = {"z\UAG_Units\addons\uag_units_uag\data\clothing1_mtp_urban_co_uag.paa"};
+        picture = "z\UAG_Units\addons\uag_units_uag\data\icon_U_B_CTRG_Soldier_urb_1_F_ca_uag.paa";
+
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply80";
+            mass = 0;
+            uniformClass = "UAG_Combat_Fatigues_Soldier";
+        };
+    };
+};
+
+class CfgVehicles {
+    class B_CTRG_Soldier_urb_1_F;
+
+    class UAG_Combat_Fatigues_Soldier: B_CTRG_Soldier_urb_1_F {
+        hiddenSelectionsTextures[] = {"z\UAG_Units\addons\uag_units_uag\data\clothing1_mtp_urban_co_uag.paa"};
+        picture = "z\UAG_Units\addons\uag_units_uag\data\icon_U_B_CTRG_Soldier_urb_1_F_ca_uag.paa";
+    };
+};
