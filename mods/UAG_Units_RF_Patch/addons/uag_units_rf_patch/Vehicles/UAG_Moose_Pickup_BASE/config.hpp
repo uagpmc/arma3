c76603778aae9ca8989b382347ecf81ce0d9f229
zuedev <zuedev@gmail.com>
2024-06-10 15:55:52 +0100

    different moose cars

---
+++ mods/UAG_Units_RF_Patch/addons/uag_units_rf_patch/Vehicles/UAG_Moose_Pickup_BASE/config.hpp
@@ -0,0 +1,12 @@
+class CfgVehicles {
+    class I_E_Pickup_rf;
+
+    class UAG_Moose_Pickup_BASE: I_E_Pickup_rf {
+        scope = 0;
+        displayName = "Moose Pickup";
+        textureList[] = {};
+        crew = "UAG_Contractor";
+        faction = "UAG";
+        side = 1;
+    };
+};
---
+++ mods/UAG_Units_RF_Patch/addons/uag_units_rf_patch/Vehicles/UAG_Moose_Pickup_Black_Splinter/config.hpp
@@ -0,0 +1,16 @@
+class CfgVehicles {
+    class UAG_Moose_Pickup_BASE;
+
+    class UAG_Moose_Pickup_Black_Splinter: UAG_Moose_Pickup_BASE {
+        scope = 2
+        displayName = "Moose Pickup (Black Splinter)";
+        hiddenSelectionsTextures[] = {
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup\pickup_01_service_ldf_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup\pickup_01_adds_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup\pickup_01_ext2_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup\pickup_01_aat_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup\pickup_01_launcher_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup\pickup_01_ext_ldf_co_uag.paa"
+        };
+    };
+};
