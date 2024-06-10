5c53ae114efa9aed3239400b8324898e77e44505
zuedev <zuedev@gmail.com>
2024-06-10 17:30:13 +0100

    add animation lists to each vehicle

---
+++ mods/UAG_Units_RF_Patch/addons/uag_units_rf_patch/Vehicles/UAG_Moose_Pickup_HMG_Black/config.hpp
@@ -0,0 +1,16 @@
+class CfgVehicles {
+    class UAG_Moose_Pickup_HMG_BASE;
+
+    class UAG_Moose_Pickup_HMG_Black: UAG_Moose_Pickup_HMG_BASE {
+        scope = 2;
+        displayName = "Moose Pickup (Black, HMG)";
+        hiddenSelectionsTextures[] = {
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup_Black\pickup_01_ext_black_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup_Black\pickup_01_adds_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup_Black\pickup_01_ext2_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup_Black\pickup_01_aat_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup_Black\pickup_01_launcher_co_uag.paa",
+            "z\UAG_Units_RF_Patch\addons\uag_units_rf_patch\Vehicles\UAG_Moose_Pickup_Black\pickup_01_service_black_co_uag.paa"
+        };
+    };
+};
