3b7d9fc50dcbffcd77488deee9fee946d1c7334a
Alex <zuedev@gmail.com>
2024-06-21 21:08:35 +0100

    use experimental --expsqfc option

--- scripts/hemttReleaseAll.ps1
+++ scripts/hemttReleaseAll.ps1
@@ -5,7 +5,7 @@ $modDirs = Get-ChildItem -Directory -Path .\ -Exclude .git
 foreach ($modDir in $modDirs) {
     Push-Location $modDir.FullName
     Write-Host "Running HEMTT Release for $($modDir.Name)"
-        & hemtt release --no-archive
+        & hemtt release --no-archive --expsqfc
     Pop-Location
 }
 
