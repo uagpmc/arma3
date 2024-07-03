e55cbc2d9496e902adb45b84f2975ab4fd2c4390
Alex <zuedev@gmail.com>
2024-07-04 00:10:47 +0100

    make marksman not suck doodoo

--- mods/UAG_Units/addons/uag_units_uag/Units/Combat_Contractors_Black_Splinter/UAG_Marksman_Black_Splinter.hpp
+++ mods/UAG_Units/addons/uag_units_uag/Units/Combat_Contractors_Black_Splinter/UAG_Marksman_Black_Splinter.hpp
@@ -4,7 +4,7 @@ class CfgVehicles {
     class UAG_Marksman_Black_Splinter: UAG_Contractor_Black_Splinter {
         displayName = "Marksman";
         magazines[] = {
-            x4("20Rnd_762x51_Mag_Tracer"),
+            x4("20Rnd_762x51_Mag"),
             "HandGrenade",
             "SmokeShell"
         };
