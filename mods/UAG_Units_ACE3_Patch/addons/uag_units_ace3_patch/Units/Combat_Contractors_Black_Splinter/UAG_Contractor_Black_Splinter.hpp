cda89455fe6200cfd1873557c289df3cfd876a5e
Alex <zuedev@gmail.com>
2024-08-06 04:13:22 +0100

    use += array concat for uag ace mod

--- mods/UAG_Units_ACE3_Patch/addons/uag_units_ace3_patch/Units/Combat_Contractors_Black_Splinter/UAG_Contractor_Black_Splinter.hpp
+++ mods/UAG_Units_ACE3_Patch/addons/uag_units_ace3_patch/Units/Combat_Contractors_Black_Splinter/UAG_Contractor_Black_Splinter.hpp
@@ -4,7 +4,7 @@ class CfgVehicles {
 
     class UAG_Contractor_Black_Splinter: UAG_Survivor_Black_Splinter {
         displayName = "Contractor";
-        items[] = {
+        items[] += {
             baseAceMedicalItems,
             baseAceMiscItems
         };
